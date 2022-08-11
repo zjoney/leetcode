c2dc09deb0b47afaacce46fb5fce1c80caf1e74d
zjoney <zdjoey@126.com>
2022-08-11 10:24:37 +0800

    💥 feat(): fix

--- 1、动态规划.js
+++ 1、动态规划.js
@@ -1,13 +1,14 @@
 
 /**
+ * @difficulty中等
  * @description: 62. 不同路径
  * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
-机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
-问总共有多少条不同的路径
+   机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+   问总共有多少条不同的路径
  * 输入：m = 3, n = 7
-输出：28
+ * 输出：28
  */
- var uniquePath = (m, n) => {
+ var uniquePaths = (m, n) => {
   // 1定义数组含义
   let dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
   // 2 找出关系数组元素间的关系式
@@ -25,4 +26,4 @@
   }
   return dp[m - 1][n - 1];
 }
-console.log(uniquePath(3, 7)) // 28
+console.log(uniquePaths(3, 7)) // 28
--- 3.动态规划Ⅲ.js
+++ 3.动态规划Ⅲ.js
@@ -1,4 +1,5 @@
 /**
+ * @difficulty困难
  * @description: 72. 编辑距离
  * 给你两个单词 word1 和 word2， 请返回将 word1 转换成 word2 所使用的最少操作数  。
 你可以对一个单词进行如下三种操作：
