915730dde4414948be3fe3efeff994a28e9b4c58
zjoney <zdjoey@126.com>
2022-08-11 17:25:23 +0800

    💥 feat(): daily-fix

--- history/1.不同路径.js
+++ history/1.不同路径.js
@@ -13,12 +13,8 @@
   // 1定义数组含义
   let dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
   // 2 找出关系数组元素间的关系式
-  for (let i = 0; i < m; i++) {
-    dp[i][0] = 1;
-  }
-  for (let i = 0; i < n; i++) {
-    dp[0][i] = 1;
-  }
+  for (let j = 0; j < n; j++) dp[0][j] = 1;
+  for (let i = 0; i < m; i++) dp[i][0] = 1;
   // 3找出初始值
   for (let i = 1; i < m; i++) {
     for (let j = 1; j < n; j++) {
