2ace728f59acb8290507223aa95689b205fdf3be
zjoney <zdjoey@126.com>
2022-08-12 15:15:53 +0800

    💥 feat(): 动态规划

--- 1.动态规划.js
+++ 1.动态规划.js
@@ -8,7 +8,19 @@
  * 输出：28
  */
 var uniquePaths = (m, n) => {
-  // TODO
+  // 定义数组含义
+  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0))
   
+  // 初始化最上、最右排
+  for(j=0; j<n; j++) dp[0][j] = 1;
+  for(i=0; i<m; i++) dp[i][0] = 1;
+
+  // 找出对应关系
+  for(i=1; i<m; i++){
+    for(j=1; j<n; j++){
+     dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    }
+  }
+  return dp[m-1][n-1];
 }
-console.log(uniquePaths(3, 7));
+console.log(uniquePaths(3, 7));// 28
