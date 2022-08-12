2adfe87f0d9a753b0025e5722f0cf1eb422dec49
zjoney <zdjoey@126.com>
2022-08-12 17:23:01 +0800

    💥 feat(): 卖出价位一定比买的价位高

--- history/9.买卖股票的最佳时机.js
+++ history/9.买卖股票的最佳时机.js
@@ -12,7 +12,19 @@
 输出：5
 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
  */
-var maxProfit = function(prices) {
-
+var maxProfit = function (prices) {
+  const len = prices.length;
+  //  定义数组
+  const dp = new Array(len).fill([0, 0]);
+  // 初始化
+  dp[0] = [-prices[0], 0]
+  for (let i = 1; i < len; i++) {
+    dp[i] = [
+      Math.max(dp[i - 1][0], -prices[i]),
+      Math.max(dp[i - 1][1], prices[i] + dp[i - 1][0])
+    ]
+  }
+  return dp[len - 1][1];
+  
 };
-console.log(maxProfit([7,1,5,3,6,4]));// 5
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
