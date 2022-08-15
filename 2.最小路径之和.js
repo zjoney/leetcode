176f48c6c8d80bb878b987ef2d76242a76d3e7ed
zjoney <zdjoey@126.com>
2022-08-15 11:40:18 +0800

    💥 feat(): 找关系

--- 2.最小路径之和.js
+++ 2.最小路径之和.js
@@ -9,7 +9,14 @@
 输出：7
 解释：因为路径 1→3→1→1→1 的总和最小。
  */
-var minPathSum=function(arr){
+var minPathSum = function (arr) {
+  const m = arr.length;
+  const n = arr[0].length;
+  const dp = new Array(m).fill(0).map(()=>new Array(n).fill(0));
+  //  初始化第一行、第一列
+  for(let j=1; j<n; j++) dp[0][j] = 1
+  for(let i=1; i<m; i++) dp[i][0] = 1
+  // 找对关系，填充数组
 
 };
-console.log(minPathSum([[1,3,1],[1,5,1],[4,2,1]]));// 7
+console.log(minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]));// 7
