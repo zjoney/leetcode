792ac7ff7494224aa20d299c1257d741cdf7f8a8
zjoney <zdjoey@126.com>
2022-08-15 14:54:57 +0800

    💥 feat(): 字符串转换最少操作数

---
+++ 3.编辑距离.js
@@ -0,0 +1,24 @@
+
+/**
+ * 
+ * 考察：动态规划
+ * @difficulty困难
+ * @description: 72. 编辑距离
+ * 给你两个单词 word1 和 word2， 请返回将 word1 转换成 word2 所使用的最少操作数  。
+你可以对一个单词进行如下三种操作：
+插入一个字符
+删除一个字符
+替换一个字符
+
+栗子:
+输入：word1 = "horse", word2 = "ros"
+输出：3
+解释：
+horse -> rorse (将 'h' 替换为 'r')
+rorse -> rose (删除 'r')
+rose -> ros (删除 'e')
+ */
+var minDistance = function (word1, word2) {
+
+}
+console.log(minDistance('rosy', 'horse')); //3
