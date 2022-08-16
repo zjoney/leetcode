c8cc71ae79c462afb81f7f201cc8742bbf7891e4
zhangjing520 <zdjoey@126.com>
2022-08-16 21:49:11 +0800

    💥 feat(): H指数-排序+二分查找法

---
+++ history/12.H指数.js
@@ -0,0 +1,36 @@
+
+/**
+ * 
+ * 考察：二分查找法+排序
+ * @difficulty中等
+ * @description: 274. H 指数
+ * 给你一个整数数组 citations ，其中 citations[i] 表示研究者的第 i 篇论文被引用的次数。计算并返回该研究者的 h 指数。
+根据维基百科上 h 指数的定义：h 代表“高引用次数”，一名科研人员的 h指数是指他（她）的 （n 篇论文中）总共有 h 篇论文分别被引用了至少 h 次。且其余的 n - h 篇论文每篇被引用次数 不超过 h 次。
+如果 h 有多种可能的值，h 指数 是其中最大的那个。
+
+示例：
+输入：citations = [3,0,6,1,5]
+输出：3 
+解释：给定数组表示研究者总共有 5 篇论文，每篇论文相应的被引用了 3, 0, 6, 1, 5 次。
+     由于研究者有 3 篇论文每篇 至少 被引用了 3 次，其余两篇论文每篇被引用 不多于 3 次，所以她的 h 指数是 3。
+ * 分析：
+ * 二分查找目标是查找后半段的左边界
+后半段的性质为：citations[i] >= citations.length - i
+ */
+
+var hIndex = function (citations) {
+  // 快速排序或桶排序
+  citations.sort((a, b) => a - b);
+  // 下半部的左边界: citations[i] >= citations.length - i
+  let l = 0,
+      r = citations.length - 1;
+  while (l < r) {
+      let m = l + r >> 1;
+      if (citations[m] >= citations.length - m) r = m;
+      else l = m + 1;
+  }
+
+  let h = citations.length - l;
+  return citations[l] >= h ? h : 0;
+};
+console.log(hIndex([3, 0, 6, 1, 5]));// 3
