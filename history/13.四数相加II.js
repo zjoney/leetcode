c4be822c803fb7dd3952df2bcef7f1ccfd4d3ea3
zjoney <zdjoey@126.com>
2022-08-18 13:49:16 +0800

    💥 feat(): 哈希表+分组

--- history/13.四数相加II.js
+++ history/13.四数相加II.js
@@ -16,10 +16,28 @@ nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
 2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
 
 分析：
-1. 暴力法：四层循环，时间复杂度O(n^4)
-2. 哈希表：两层循环，将两个数组的和存入哈希表，时间复杂度O(n^2)
+1. 哈希表：两层循环，将两个数组的和存入哈希表，时间复杂度O(n^2)
  */
-var fourSumCount = function(nums1, nums2, nums3, nums4) {
 
+var fourSumCount = function (nums1, nums2, nums3, nums4) {
+  let abMap = new Map();
+  for (let i of nums1) {
+    for (let j of nums2) {
+      if (abMap.has(i + j)) {
+        abMap.set(i + j, abMap.get(i + j) + 1);
+      } else {
+        abMap.set(i + j, 1);
+      }
+    }
+  }
+  let res = 0;
+  for (let m of nums3) {
+    for (let n of nums4) {
+      if (abMap.has(-(m + n))) {
+        res += abMap.get(-(m + n));
+      }
+    }
+  }
+  return res;
 };
-console.log(fourSumCount([1,2],[3,4],[5,6],[7,8]));// 1
+console.log(fourSumCount([1, 2], [-2, -1], [-1, 2], [0, 2]));// 2
