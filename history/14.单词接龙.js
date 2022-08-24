f8eb820924393bf132612a4192b3bdee5c78be44
zjoney <zdjoey@126.com>
2022-08-24 10:41:32 +0800

    💥 feat():单词接龙

---
+++ history/14.单词接龙.js
@@ -0,0 +1,22 @@
+/**
+ * 考察：
+ * @difficulty困难
+ * @description: 127. 单词接龙
+ * 字典 wordList 中从单词 beginWord 和 endWord 的 转换序列 是一个按下述规格形成的序列 beginWord -> s1 -> s2 -> ... -> sk：
+
+每一对相邻的单词只差一个字母。
+ 对于 1 <= i <= k 时，每个 si 都在 wordList 中。注意， beginWord 不需要在 wordList 中。
+sk == endWord
+给你两个单词 beginWord 和 endWord 和一个字典 wordList ，返回 从 beginWord 到 endWord 的 最短转换序列 中的 单词数目 。如果不存在这样的转换序列，返回 0 。
+示例：
+输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+输出：5
+解释：一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog", 返回它的长度 5。
+题目理解：
+
+ */
+var ladderLength = function (beginWord, endWord, wordList) {
+  let ant;
+  return ant;
+};
+console.log(ladderLength('hit', 'cog', ["hot", "dot", "dog", "lot", "log", "cog"])); // 5
