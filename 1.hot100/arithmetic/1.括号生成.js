b9643cacfda3068f937cb4ecf26cba016e43ce95
zjoney <zdjoey@126.com>
2022-09-02 15:50:03 +0800

    💥 feat(算法):fix- 回溯

--- 1.hot100/arithmetic/1.括号生成.js
+++ 1.hot100/arithmetic/1.括号生成.js
@@ -6,11 +6,14 @@
  * 输入：n = 3
 输出：["((()))","(()())","(())()","()(())","()()()"]
 提示1<n<8
+2 2->1 2/0 2/-------0 1/0 0 
+回退：0 1/0 2/1 2
+1 2->1 1/0 1/---1 2
  */
 var generateParenthesis = function (n) {
   const res = [];
-
   const dfs = (lRemain, rRemain, str) => { // 左右括号所剩的数量，str是当前构建的字符串
+    debugger;
     if (str.length == 2 * n) { // 字符串构建完成
       res.push(str);           // 加入解集
       return;                  // 结束当前递归分支
@@ -22,10 +25,10 @@ var generateParenthesis = function (n) {
       dfs(lRemain, rRemain - 1, str + ")"); // 然后继续做选择（递归）
     }
   };
-
+  debugger;
   dfs(n, n, ""); // 递归的入口，剩余数量都是n，初始字符串是空串
   return res;
 };
 
-const n = 3
-console.log(generateParenthesis(n)); // ["((()))","(()())","(())()","()(())","()()()"]
+const n = 2 // [ '(())', '()()' ]
+console.log(generateParenthesis(n)); //3-> ["((()))","(()())","(())()","()(())","()()()"]
