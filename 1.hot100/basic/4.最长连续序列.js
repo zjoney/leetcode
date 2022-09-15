bc317b8b533e0688594f7d24f0d20d8f8e86a0a9
zjoney <zdjoey@126.com>
2022-09-15 13:36:22 +0800

    feat: 🎸 hot100
    题目分析

--- 1.hot100/basic/4.最长连续序列.js
+++ 1.hot100/basic/4.最长连续序列.js
@@ -8,11 +8,15 @@
 输入：nums = [100,4,200,1,3,2]
 输出：4
 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+题目理解：
+使用Set结构，方便查找与删除；
+同一段连续序列内的数值，他们的最长连续序列长度是一样的，所以剩余的都删除；
+计数，同时也可计算长度 cur - value -1；
 
  */
 var longestConsecutive = function (nums) {
   debugger;
-  // 使用Set结构，方便查找与删除；
+  //  把题目中数组的数字全部放入set中，一来去重，二来方便快速查找；
   let num_set = new Set();
   for (const num of nums) {
     num_set.add(num);
@@ -21,18 +25,16 @@ var longestConsecutive = function (nums) {
   let longestStreak = 0;
 
   for (const num of num_set) {
-    // 从最小的开始查找
-    // 从没有比自己小1的数开始查找连续序列
-    // 这样可以单向处理
+    // 没有左邻居，是序列的起点
     if (!num_set.has(num - 1)) {
       let currentNum = num;
       let currentStreak = 1;
-      // 开始逐步生长
+      // 有右邻居，看连续的右邻居有多少个
       while (num_set.has(currentNum + 1)) {
         currentNum += 1;
         currentStreak += 1;
       }
-
+      // 存放最大的连续邻居的值
       longestStreak = Math.max(longestStreak, currentStreak);
     }
   }
