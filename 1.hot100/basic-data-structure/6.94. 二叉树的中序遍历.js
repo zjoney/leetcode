18911173025d21b3fafb71426ba625e7a684b8a2
zjoney <zdjoey@126.com>
2022-09-19 14:33:37 +0800

    feat: 🎸 hot100
    基础数据结构
    
    BREAKING CHANGE: 🧨 递归思想

--- 1.hot100/basic-data-structure/6.94. 二叉树的中序遍历.js
+++ 1.hot100/basic-data-structure/6.94. 二叉树的中序遍历.js
@@ -8,7 +8,22 @@
 输入：root = [1,null,2,3]
 输出：[1,3,2]
  */
-var inorderTraversal = function (root) {
+// 递归
+var inorderTraversal = function(root) { 
+  const res = [];
+  const inorder = (root) => {
+      if (!root) {
+          return;
+      }
+      inorder(root.left);
+      res.push(root.val);
+      inorder(root.right);
+  }
+  inorder(root);
+  return res;
+};
+//迭代 
+var inorderTraversal2 = function (root) {
   const res = [];
   const stk = [];
   while (root || stk.length) {
