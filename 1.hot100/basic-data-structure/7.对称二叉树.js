d7dc4252a1c00cc1a0cb824f13627572625a6f9a
zjoney <zdjoey@126.com>
2022-09-20 13:56:50 +0800

    feat: 🎸 对称二叉树
    递归

--- 1.hot100/basic-data-structure/7.对称二叉树.js
+++ 1.hot100/basic-data-structure/7.对称二叉树.js
@@ -8,8 +8,13 @@
  * 输入：root = [1,2,2,3,4,4,3]
 输出：true
  */
-var isSymmetric= function(root){
-
+const check = (p, q) => {
+  if (!p && !q) return true;
+  if (!p || !q) return false;
+  return p.val === q.val && check(p.left, q.right) && check(p.right, q.left);
 }
+var isSymmetric = function(root) {
+  return check(root, root);
+};
 const root = [1,2,2,3,4,4,3]
 console.log(isSymmetric(root));
