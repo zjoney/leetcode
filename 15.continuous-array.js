0f819a13027d374704239a19e27c82434e62ddac
zjoney <zdjoey@126.com>
2022-09-21 09:38:16 +0800

    feat: 🎸 daily
    连续数组

---
+++ 15.continuous-array.js
@@ -0,0 +1,15 @@
+/**
+ * 考察:哈希表
+ * @difficulty中等
+ * @summary525. 连续数组
+ * 给定一个二进制数组 nums , 找到含有相同数量的 0 和 1 的最长连续子数组，并返回该子数组的长度。
+输入: nums = [0,1]
+输出: 2
+说明: [0, 1] 是具有相同数量 0 和 1 的最长连续子数组
+ */
+
+var findMaxLength = function (nums) {
+
+};
+const nums = [0, 1]
+console.log(findMaxLength(nums));
