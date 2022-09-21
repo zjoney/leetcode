bc4ca297360a104461db1496233043d92185bd98
zjoney <zdjoey@126.com>
2022-09-21 09:38:48 +0800

    feat: 🎸 diaily
    哈希表

--- 15.continuous-array.js
+++ 15.continuous-array.js
@@ -8,8 +8,27 @@
 说明: [0, 1] 是具有相同数量 0 和 1 的最长连续子数组
  */
 
-var findMaxLength = function (nums) {
-
+var findMaxLength = function(nums) {
+  let maxLength = 0;
+  const map = new Map();
+  let counter = 0;
+  map.set(counter, -1);
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+      const num = nums[i];
+      if (num == 1) {
+          counter++;
+      } else {
+          counter--;
+      }
+      if (map.has(counter)) {
+          const prevIndex = map.get(counter);
+          maxLength = Math.max(maxLength, i - prevIndex);
+      } else {
+          map.set(counter, i);
+      }
+  }
+  return maxLength;
 };
 const nums = [0, 1]
 console.log(findMaxLength(nums));
