ac143f8a2ed20a6ac31b6ea69366edfdbe13f53e
zjoney <zdjoey@126.com>
2022-09-28 10:07:19 +0800

    feat: 🎸 answer for daily temperature

--- 1.hot100/arithmetic/4.daily-temp.js
+++ 1.hot100/arithmetic/4.daily-temp.js
@@ -7,8 +7,20 @@
 输出: [1,1,4,2,1,1,0,0]
 
  */
-var dailyTemperatures = function(temperatures) {
-
+var dailyTemperatures = function (temperatures) {
+  //栈
+  let n = temperatures.length;
+  let answer = new Array(n).fill(0);//初始化，假设后续都没出现过更高气温
+  let stack = [];
+  for (let i = 0; i < n; i++) {//遍历数组确定answer[i]
+    while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {//栈中有元素，且前几天有比当前温度低的情况
+      let preIndex = stack.pop();//已经找到比第preIndex天温度高的是哪天了，answer[preIndex]已经确定了，把preIndex从栈里pop出来
+      answer[preIndex] = i - preIndex;//记录第preIndex天后出现高温距preIndex有多少天
+    }
+    stack.push(i);//answer[i]还不知道，先把i压进栈
+  }
+  return answer;
 };
-const temperatures = [73,74,75,71,69,72,76,73]
+
+const temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
 console.log(dailyTemperatures(temperatures));
