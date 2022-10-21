7360d42b9bf4701084e019ce2cfb03644abd3d21
zjoney <zdjoey@126.com>
2022-10-21 09:29:27 +0800

    feat: 🎸 debug

--- 1.hot100/basic/6.rotate-the-image.js
+++ 1.hot100/basic/6.rotate-the-image.js
@@ -8,13 +8,12 @@
 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
 输出：[[7,4,1],[8,5,2],[9,6,3]]
  */
-var rotate = function(matrix) {var rotate = function(matrix) {
+var rotate = function(matrix) {
   const n = matrix.length;
   const matrix_new = new Array(n).fill(0).map(() => new Array(n).fill(0));
   for (let i = 0; i < n; i++) {
       for (let j = 0; j < n; j++) {
-          matrix_new[j][n - i - 1] = matrix[i][j]; // 翻转之后就是倒数第i行和第i列
-
+          matrix_new[j][n - i - 1] = matrix[i][j];
       }
   }
   for (let i = 0; i < n; i++) {
@@ -22,7 +21,6 @@ var rotate = function(matrix) {var rotate = function(matrix) {
           matrix[i][j] = matrix_new[i][j];
       }
   }
+  console.log('matrix_new', matrix_new); // [ [ 7, 4, 1 ], [ 8, 5, 2 ], [ 9, 6, 3 ] ]
 };
-};
-const matrix = [[1,2,3],[4,5,6],[7,8,9]];
-console.log(roate(matrix));;
+rotate([[1,2,3],[4,5,6],[7,8,9]])
