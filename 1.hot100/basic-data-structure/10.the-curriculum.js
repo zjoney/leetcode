f7023075031125b9c3ec6b0d7f15a51975bd1348
zjoney <zdjoey@126.com>
2022-11-01 09:18:42 +0800

    perf: ⚡️ remove log

--- 1.hot100/basic-data-structure/10.the-curriculum.js
+++ 1.hot100/basic-data-structure/10.the-curriculum.js
@@ -20,7 +20,7 @@ image/10.the-curriculum.png
 
  */
 var canFinish = (numCourses, prerequisites) => {
-  debugger
+  
   const inDegree = new Array(numCourses).fill(0); // 入度数组
   const map = {};                                 // 邻接表
   for (let i = 0; i < prerequisites.length; i++) {
@@ -36,7 +36,7 @@ var canFinish = (numCourses, prerequisites) => {
     if (inDegree[i] == 0) queue.push(i);
   }
   let count = 0;
-  debugger
+  
   while (queue.length) {
     const selected = queue.shift();           // 当前选的课，出列
     count++;                                  // 选课数+1
