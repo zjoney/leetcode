52d728e4086bdd61e7d88a95b504156a7c1aa9d9
zhangjing520 <zdjoey@126.com>
2022-11-05 09:31:36 +0800

    💥 feat():  update jump

--- 1.hot100/arithmetic/6.jump-games.js
+++ 1.hot100/arithmetic/6.jump-games.js
@@ -13,9 +13,8 @@
 
  */
  var canJump = function(nums) {
-
+  // 必须到达end下标的数字
   let end = nums.length - 1;
-
   for (let i = nums.length - 2; i >= 0; i--) {
       if (end - i <= nums[i]) {
           end = i;
