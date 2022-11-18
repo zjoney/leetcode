2af681f6e2fec8d33d9763ad597f3c3204c150d2
zhangjing520 <zdjoey@126.com>
2022-11-18 09:49:03 +0800

    💥 feat(): analisy

--- 1.hot100/basic-data-structure/12.delete-the-nth-to-last-node-of-the-linked-list.js
+++ 1.hot100/basic-data-structure/12.delete-the-nth-to-last-node-of-the-linked-list.js
@@ -2,6 +2,11 @@
  * 考察：链表
  * @difficulty 困难
  * @summary:19. 删除链表的倒数第 N 个结点
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * 如图images/remove-nth-link.jpg
+ * 示例一：
+ * 输入：head = [1,2,3,4,5], n = 2
+输出：[1,2,3,5]
  */
  var removeNthFromEnd = function(head, n) {
 
