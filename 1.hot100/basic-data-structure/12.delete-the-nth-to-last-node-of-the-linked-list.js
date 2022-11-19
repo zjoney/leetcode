241c61aa010145179ae0a9d95e4e7a13bee11e30
zhangjing520 <zdjoey@126.com>
2022-11-19 09:41:44 +0800

    💥 feat(): analisy

--- 1.hot100/basic-data-structure/12.delete-the-nth-to-last-node-of-the-linked-list.js
+++ 1.hot100/basic-data-structure/12.delete-the-nth-to-last-node-of-the-linked-list.js
@@ -8,7 +8,16 @@
  * 输入：head = [1,2,3,4,5], n = 2
 输出：[1,2,3,5]
  */
-var removeNthFromEnd = function (head, n) {
-  
+var removeNthFromEnd = function(head, n) {
+  let ret = new ListNode(0, head),
+      slow = fast = ret;
+  while(n--) fast = fast.next;
+  if(!fast) return ret.next;
+  while (fast.next) {
+      fast = fast.next; 
+      slow = slow.next
+  };
+  slow.next = slow.next.next;
+  return ret.next;
 };
 
