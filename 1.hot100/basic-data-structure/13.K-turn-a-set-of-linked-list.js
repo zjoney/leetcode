b4b42e72c461be70ef69ab751a854259e8b41c6e
zjoney <zdjoey@126.com>
2022-11-29 09:57:10 +0800

    feat: 🎸 update

--- 1.hot100/basic-data-structure/13.K-turn-a-set-of-linked-list.js
+++ 1.hot100/basic-data-structure/13.K-turn-a-set-of-linked-list.js
@@ -11,7 +11,18 @@ k 是一个正整数，它的值小于或等于链表的长度。如果节点总
 
  */
 var reverseKGroup = function (head, k) {
-  
+  if(!head) return null;
+  let start = head, end = head;
+
+  for(let i = 0; i < k; i++) {
+      // 如果在还没遍历到第 k 个，end 空了，即 head 链表个数不满足 k 个，直接返回原链表
+      if(!end) {
+          return head;
+      } else {
+          end = end.next;
+      }
+  }
+
   let newHead = reverse(start, end); // 左闭右开区间
   start.next = reverseKGroup(end, k); // 翻转以后，原本的 start 指向的结点，变到了 end 的前一个，直接 start.next 继续递归翻转后续的就行
   return newHead;
