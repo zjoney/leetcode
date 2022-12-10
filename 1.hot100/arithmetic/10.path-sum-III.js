f523dc739292b9b54235bff1f34c5ae01ed2193c
zhangjing520 <zdjoey@126.com>
2022-12-10 17:44:12 +0800

    💥 feat(): analysis for pathsumIII

--- 1.hot100/arithmetic/10.path-sum-III.js
+++ 1.hot100/arithmetic/10.path-sum-III.js
@@ -6,7 +6,12 @@
 路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
 查看images/pathsumIII.jpg
  */
-// 方法一：
+/**
+*方法一：
+ *首先定义 rootSum(p,val)\textit{rootSum}(p,\textit{val})rootSum(p,val) 表示以节点 ppp 为起点向下且满足路径总和为 valvalval 的路径数目
+ *对节点 ppp 求 rootSum(p,targetSum)\textit{rootSum}(p,\textit{targetSum})rootSum(p,targetSum) 时
+ *左孩子节点pl求出 rootSum(pl,targetSum−val)，右孩子节点pr=rootSum(pr,targetSum−val)
+ */
 var pathSum = function(root, targetSum) {
   if(!root) return 0
   let curNode = dfs(root,targetSum)
