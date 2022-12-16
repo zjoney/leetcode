430e787ac7369df9bf4361af16b67af89a8a11b6
zjoney <zdjoey@126.com>
2022-12-16 09:42:45 +0800

    feat: 🎸 deep

--- 1.hot100/arithmetic/11.word-search.js
+++ 1.hot100/arithmetic/11.word-search.js
@@ -9,6 +9,33 @@
 输出：true
 
  */
-var exist = function(board, word) {
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+ var exist = function(board, word) {
+  let n = board.length;
+  let m = board[0].length;
+  let wLen = word.length;
+  const findWord = (i, j, curLen) => {//进行深度优先搜索
+      if(curLen === wLen) return true;//已经检查完word字符，说明全部符合
+      if(i >= n || j >= m || i < 0 || j < 0) return false;//数组越界
+      if(board[i][j] !== word.charAt(curLen)) return false;//当前位置储存字符不等于当前word字符
+      let pre = board[i][j];//储存当前位置字符
+      board[i][j] = '0';//修改字符为不可能匹配值，防止之后深度优先搜索又经过这个字符
+      let res = findWord(i + 1, j, curLen + 1) || //朝不同方向搜索到底
+                findWord(i, j + 1, curLen + 1) ||
+                findWord(i - 1, j, curLen + 1) ||
+                findWord(i, j - 1, curLen + 1);
+      board[i][j] = pre;//将字符换回来
+      return res;
 
+  }
+  for(let i = 0; i < n; i++) {//找到数组中符合word第一个字符的位置
+      for(let j = 0; j < m; j++) {
+          if(findWord(i, j, 0)) return true;
+      }
+  }
+  return false;
 };
