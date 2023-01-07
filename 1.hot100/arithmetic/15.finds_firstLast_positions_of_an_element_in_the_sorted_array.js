e722801839022f4c00eb89806a74afa1aa1e9011
zhangjing520 <zdjoey@126.com>
2023-01-07 22:41:56 +0800

    💥 feat(): finds first last

--- 1.hot100/arithmetic/15.finds_firstLast_positions_of_an_element_in_the_sorted_array.js
+++ 1.hot100/arithmetic/15.finds_firstLast_positions_of_an_element_in_the_sorted_array.js
@@ -3,9 +3,7 @@
  * @difficulty 中等
  * @summary:34. 在排序数组中查找元素的第一个和最后一个位置
  * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
-
 如果数组中不存在目标值 target，返回 [-1, -1]。
-
 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
  */
 var searchRange = function(nums, target) {
@@ -16,6 +14,7 @@ var searchRange = function(nums, target) {
   
   // 迭代查找右边界
   while (left <= right) {
+    debugger
     let mid = left + Math.floor((right - left) / 2);
     
     if (nums[mid] === target && (mid === len - 1 || nums[mid + 1] > target)) {
