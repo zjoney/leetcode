e8fe3f01123b46a16d203e34cfcede2ade3aa567
zjoney <zdjoey@126.com>
2023-01-13 09:07:28 +0800

    feat: 🎸 merge binary

--- 1.hot100/arithmetic/17. merge_binary_trees.js
+++ 1.hot100/arithmetic/17. merge_binary_trees.js
@@ -13,3 +13,20 @@
 输出：[3,4,5,5,4,null,7]
 
  */
+/**
+ * DFS
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+ var mergeTrees = function (root1, root2) {
+  // 如果一棵树有，另一棵树没有，接上去
+  if (root1 == null) return root2;
+  if (root2 == null) return root1;
+  // 两棵树都有的节点，叠加节点值
+  root1.val += root2.val;
+  // 递归合并左右子树
+  root1.left = mergeTrees(root1.left, root2.left);
+  root1.right = mergeTrees(root1.right, root2.right);
+  return root1;
+};
