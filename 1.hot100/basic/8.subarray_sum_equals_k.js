e796459daf7bfaa131cd8938d1dd0f196ebdbc7b
zhangjing520 <zdjoey@126.com>
2023-01-21 15:29:45 +0800

    💥 feat():  Subarray Sum Equals K

--- 1.hot100/basic/8.subarray_sum_equals_k.js
+++ 1.hot100/basic/8.subarray_sum_equals_k.js
@@ -1,3 +1,11 @@
 /**
- * 560. 和为 K 的子数组
+ * 考察：哈希表
+ * @difficulty简单
+ * @summary:560. 和为 K 的子数组
+ * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的连续子数组的个数 。
+ * 
+ * 输入：nums = [1,1,1], k = 2
+输出：2
  */
+var subarraySum = function(nums, k) {
+};
