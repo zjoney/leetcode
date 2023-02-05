3e7ab0b1208c0d01a30b6751ec54193ef3043661
zhangjing520 <zdjoey@126.com>
2023-02-05 12:13:32 +0800

    💥 feat():  remove invalid

--- 1.hot100/arithmetic/19.remove_invalid_parentheses.js
+++ 1.hot100/arithmetic/19.remove_invalid_parentheses.js
@@ -9,41 +9,45 @@
 输入：s = "()())()"
 输出：["(())()","()()()"]
  */
-var removeInvalidParentheses=function(s){
-  // BFS广度优先,注意题目中只删除最少括号
-
-  // s在任意位置删除一个括号字符，则形成新的层级
-  // 最坏情况就是O(2^len),但是会在删除若干字符后退出
-  let level=new Set();
-  level.add(s)
-
-  while(true){
-      const valid=[...level].filter(str=>isValid(str))
-      if(valid.length) return valid;
-
-      // 
-      let next_level=new Set();
-      for(const str of level){
-          for(let i=0;i<str.length;i++){
-              if(['(',')'].includes(str[i]))
-                  next_level.add(str.slice(0,i)+str.slice(i+1))
-          }
-      }
-
-      level=next_level;
-  }
-
-  // 判定当前括号排列是否合法
-  function isValid(s){
-      let cnt=0;
-
-      for(const c of s){
-          if(c==='(') cnt++;
-          if(c===')') cnt--;
-
-          if(cnt<0) return false
-      }
-
-      return cnt===0
-  }
-}
+var removeInvalidParentheses = function (s) {
+    const n = s.length;
+    // 记录最大有效括号数量
+    let count = 0;
+    let l = 0;
+    // 存在重复字符形式
+    const set = new Set();
+    for (let c of s) {
+        if (c == '(') {
+            l++;
+        } else if (c == ')' && l > 0) {
+            l--;
+            count++;
+        }
+    }
+    const dfs = (i, l, r, str) => {
+        // 数量关系，剪枝
+        if (l < r || l > count || r > count) return;
+        // 结束
+        if (i == n) {
+            // 数量相同，删除最小
+            if (l == count && r == count) {
+                set.add(str);
+            }
+            return;
+        }
+        const cur = s[i];
+        if (cur == '(') {
+            // 括号，选或不选
+            dfs(i + 1, l + 1, r, str + '(');
+            dfs(i + 1, l, r, str);
+        } else if (cur == ')') {
+            dfs(i + 1, l, r + 1, str + ')');
+            dfs(i + 1, l, r, str);
+        } else {
+            // 字符直接加入
+            dfs(i + 1, l, r, str + cur);
+        }
+    }
+    dfs(0, 0, 0, '');
+    return [...set];
+};
