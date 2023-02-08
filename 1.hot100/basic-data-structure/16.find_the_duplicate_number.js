52b5037b6b477746a470411704ee2a84ce5a6a5d
zjoney <zdjoey@126.com>
2023-02-08 09:29:39 +0800

    feat: 🎸 find the dupplicate number

--- 1.hot100/basic-data-structure/16.find_the_duplicate_number.js
+++ 1.hot100/basic-data-structure/16.find_the_duplicate_number.js
@@ -15,6 +15,21 @@
  * 方法1
  * @param {*} nums 
  */
-var findDuplicate = function(nums) {
+ var findDuplicate = function(nums) {
+  let fast = 0;
+  let slow = 0;
 
+  while (1) {
+    fast = nums[nums[fast]];
+    slow = nums[slow];
+
+    if (slow === fast) {
+      fast = 0;
+      while(nums[slow] != nums[fast]) {
+        fast = nums[fast];
+        slow = nums[slow];
+      }
+      return nums[slow];
+    }
+  }
 };
