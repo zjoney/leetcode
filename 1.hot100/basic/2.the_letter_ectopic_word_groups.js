d056c59eb62fc670a26eb8d19fd5f648b8e4ca5e
zjoney <zdjoey@126.com>
2023-02-09 10:40:20 +0800

    perf: ⚡️ adjust for hoot100 basic

--- 1.hot100/basic/2.the_letter_ectopic_word_groups.js
+++ 1.hot100/basic/2.the_letter_ectopic_word_groups.js
@@ -3,13 +3,13 @@
  * @difficulty中等
  * @summary: 49.字母异位词分组
  * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
-字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
-输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
-题目理解：
-1、创建一个对象map
-2、循环传入的数组，取到的每一个值分别单独作为map的key、value
-3、给每一个key值升序转字符串，每一个value放在数组list
+ * 字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * 题目理解：
+ * 1、创建一个对象map
+ * 2、循环传入的数组，取到的每一个值分别单独作为map的key、value
+ * 3、给每一个key值升序转字符串，每一个value放在数组list
  */
 var groupAnagrams = function (strs) {
   debugger;
--- 1.hot100/basic/8.subarray_sum_equals_k.js
+++ 1.hot100/basic/8.subarray_sum_equals_k.js
@@ -5,7 +5,7 @@
  * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的连续子数组的个数 。
  * 
  * 输入：nums = [1,1,1], k = 2
-输出：2
+ * 输出：2
  */
 var subarraySum = function(nums, k) {
   let res = 0;
