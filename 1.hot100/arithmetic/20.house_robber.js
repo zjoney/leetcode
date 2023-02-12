b7465922d2901680e3ab454fe759b12184fb2606
zhangjing520 <zdjoey@126.com>
2023-02-12 18:30:10 +0800

    💥 feat():  house robber

--- 1.hot100/arithmetic/20.house_robber.js
+++ 1.hot100/arithmetic/20.house_robber.js
@@ -1,5 +1,5 @@
 /**
- * 考察：
+ * 考察：动态规划
  * @difficulty中等
  * @summary:198. 打家劫舍
  * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，
@@ -12,4 +12,24 @@
 输出：4
 解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
      偷窃到的最高金额 = 1 + 3 = 4 。
+
+分析状态转移方程： 
+1.由于两间相邻的房屋是不可以接连闯入的 那么对于dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]) 
+2.先处理基本情况 dp[0] dp[1]
+
  */
+var rob = function (nums) {
+    if (nums.length === 0) return 0;
+    if (nums.length === 1) return nums[0];
+
+    let dp = [],
+        n = nums.length;
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+
+    for (let i = 2; i < n; i++) {
+        dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+    }
+
+    return dp[n - 1];
+};
