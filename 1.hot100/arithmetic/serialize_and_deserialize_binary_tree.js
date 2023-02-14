4467fb28d2e635cd050dec0877974397895e94a5
zjoney <zdjoey@126.com>
2023-02-14 09:46:22 +0800

    feat: 🎸 description for serialize and deserialize

--- 1.hot100/arithmetic/serialize_and_deserialize_binary_tree.js
+++ 1.hot100/arithmetic/serialize_and_deserialize_binary_tree.js
@@ -2,4 +2,31 @@
  * 考察:
  * @difficulty 困难
  * @summary 297.二叉树的序列化与反序列化
+ * 序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，
+ * 同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
+ * 请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，
+ * 你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+ * 
+ * 示例一
+ * 如图 images/serdeser.jpg
+ * 输入：root = [1,2,3,null,null,4,5]
+ * 输出：[1,2,3,null,null,4,5]
+ * 
  */
+/**
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+ var serialize = function(root) {
+    
+};
+
+/**
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+ var deserialize = function(data) {
+    
+};
