6823fdba739b78d8dc6221b4cfe6ab2aceb1246f
zjoney <zdjoey@126.com>
2023-02-15 11:13:58 +0800

    feat: 🎸 deserialize

--- 1.hot100/arithmetic/21.serialize_and_deserialize_binary_tree.js
+++ 1.hot100/arithmetic/21.serialize_and_deserialize_binary_tree.js
@@ -12,13 +12,21 @@
  * 输入：root = [1,2,3,null,null,4,5]
  * 输出：[1,2,3,null,null,4,5]
  * 
+ * 题目理解：
+ * 序列化： 
+ * 使用DFS遍历每个节点。 如果遇到节点为空，则返回X。 如果节点有值，则将其和左右子树，按照根、左、右的顺序拼接成字符串。
+ * 
+ * 反序列化： 由于已经按照根、左、右的顺序，对二叉树进行了序列化，只要按照这个顺序递归生成二叉树即可。 
+ * 将序列化的字符串转换成数组，每次递归都出队一个值，就保证了根、左、右的顺序。 
+ * 如果出队的值为X，则生成一个null，并将其返回。 
+ * 如果出队的是正常值，则用其创建一个节点，并将其与递归生成的左右子树连接成二叉树。
  */
 /**
  *
  * @param {TreeNode} root
  * @return {string}
  */
- var serialize = function(root) {
+var serialize = function (root) {
   // 如果节点为空，使用一个特定的字符标识
   if (!root) {
     return 'X';
@@ -37,6 +45,29 @@
  * @param {string} data
  * @return {TreeNode}
  */
- var deserialize = function(data) {
-    
+var deserialize = function (data) {
+  // 将序列化的字符串，转换为数组
+  const valList = data.split(',');
+
+  // 生成二叉树的方法
+  function build() {
+    // 由于二叉树已经按照根、左、右的顺序序列化，每次递归只需要按顺序取出每个节点的值即可
+    const val = valList.shift();
+
+    // 如果当前值为X，表示此节点为空，直接返回null
+    if (val === 'X') {
+      return null;
+    }
+
+    // 使用当前值生成一个节点
+    const node = new TreeNode(val);
+
+    // 由于子节点都是按照先左后右的顺序取出，因此按照同样顺序将子节点连接到根节点即可
+    node.left = build();
+    node.right = build();
+
+    // 将当前生成的节点返回，供上一层递归生成树
+    return node;
+  }
+  return build();
 };
