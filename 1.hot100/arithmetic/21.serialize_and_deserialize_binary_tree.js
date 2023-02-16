96c1f81963a64c81a58292a2cb59acca2fa3a59e
zjoney <zdjoey@126.com>
2023-02-16 09:53:19 +0800

    feat: 🎸 anwser for serialize an deserialize

--- 1.hot100/arithmetic/21.serialize_and_deserialize_binary_tree.js
+++ 1.hot100/arithmetic/21.serialize_and_deserialize_binary_tree.js
@@ -22,52 +22,84 @@
  * 如果出队的是正常值，则用其创建一个节点，并将其与递归生成的左右子树连接成二叉树。
  */
 /**
- *
+ * --------------------方法2
+ * 递归dfs
+ */
+ const serialize = (root) => {
+  if (root == null) {                  //遇到null 返回‘X’进行标示
+      return 'X';
+  }
+  const left = serialize(root.left);   //序列化左子树
+  const right = serialize(root.right); //序列化右子树
+  return root.val + ',' + left + ',' + right; //按根,左,右 返回字符串
+};
+
+const deserialize = (data) => {
+  const list = data.split(',');   //字符串转数组
+
+  const buildTree = (list) => {  	//构建树
+      const rootVal = list.shift(); //第一个元素
+      if (rootVal == "X") {         //如果是X，返回null
+          return null;
+      }
+      const root = new TreeNode(rootVal); //如果不是X就创建节点
+      root.left = buildTree(list);        //构建左子树
+      root.right = buildTree(list);       //构建右子树
+      return root;                        //返回构建的节点
+  };
+
+  return buildTree(list);
+};
+
+
+
+/**
+ *--------------方法1
  * @param {TreeNode} root
  * @return {string}
  */
-var serialize = function (root) {
-  // 如果节点为空，使用一个特定的字符标识
-  if (!root) {
-    return 'X';
-  }
+// var serialize = function (root) {
+//   // 如果节点为空，使用一个特定的字符标识
+//   if (!root) {
+//     return 'X';
+//   }
 
-  // 每次递归都获取左右子树的序列化结果
-  const left = serialize(root.left);
-  const right = serialize(root.right);
+//   // 每次递归都获取左右子树的序列化结果
+//   const left = serialize(root.left);
+//   const right = serialize(root.right);
 
-  // 将当前二叉树按照根,左,右的方式拼接
-  return `${root.val},${left},${right}`;
-};
+//   // 将当前二叉树按照根,左,右的方式拼接
+//   return `${root.val},${left},${right}`;
+// };
 
 /**
  *
  * @param {string} data
  * @return {TreeNode}
  */
-var deserialize = function (data) {
-  // 将序列化的字符串，转换为数组
-  const valList = data.split(',');
+// var deserialize = function (data) {
+//   // 将序列化的字符串，转换为数组
+//   const valList = data.split(',');
 
-  // 生成二叉树的方法
-  function build() {
-    // 由于二叉树已经按照根、左、右的顺序序列化，每次递归只需要按顺序取出每个节点的值即可
-    const val = valList.shift();
+//   // 生成二叉树的方法
+//   function build() {
+//     // 由于二叉树已经按照根、左、右的顺序序列化，每次递归只需要按顺序取出每个节点的值即可
+//     const val = valList.shift();
 
-    // 如果当前值为X，表示此节点为空，直接返回null
-    if (val === 'X') {
-      return null;
-    }
+//     // 如果当前值为X，表示此节点为空，直接返回null
+//     if (val === 'X') {
+//       return null;
+//     }
 
-    // 使用当前值生成一个节点
-    const node = new TreeNode(val);
+//     // 使用当前值生成一个节点
+//     const node = new TreeNode(val);
 
-    // 由于子节点都是按照先左后右的顺序取出，因此按照同样顺序将子节点连接到根节点即可
-    node.left = build();
-    node.right = build();
+//     // 由于子节点都是按照先左后右的顺序取出，因此按照同样顺序将子节点连接到根节点即可
+//     node.left = build();
+//     node.right = build();
 
-    // 将当前生成的节点返回，供上一层递归生成树
-    return node;
-  }
-  return build();
-};
+//     // 将当前生成的节点返回，供上一层递归生成树
+//     return node;
+//   }
+//   return build();
+// };
