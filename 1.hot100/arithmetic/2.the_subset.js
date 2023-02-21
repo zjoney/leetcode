a9887f4740ac9fc992433a40c4430b35b40a23ce
zjoney <zdjoey@126.com>
2023-02-21 10:34:20 +0800

    feat: 🎸 anwser for the subset
    https://leetcode.cn/problems/subsets/submissions/404461719/?favorite=2cktkvj

--- 1.hot100/arithmetic/2.the_subset.js
+++ 1.hot100/arithmetic/2.the_subset.js
@@ -16,7 +16,7 @@ var subsets = function (nums) {
   let result = []//存放结果
   let path = []//存放一个分支的结果
   function backtracking(startIndex) {//startIndex字符递归开始的位置
-    debugger;
+    
     result.push(path.slice())//path.slice()断开和path的引用关系
     for (let i = startIndex; i < nums.length; i++) {//从startIndex开始递归
       path.push(nums[i])//当前字符推入path
