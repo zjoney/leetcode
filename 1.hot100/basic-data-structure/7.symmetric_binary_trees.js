ffa0b7d0449a3537cf9c8ff8a4b3da887bf95949
zjoney <zdjoey@126.com>
2023-02-22 10:16:04 +0800

    feat: 🎸 answer for symmetric binary tree
    https://leetcode.cn/problems/symmetric-tree/submissions/404834392/?favorite=2cktkvj

--- 1.hot100/basic-data-structure/7.symmetric_binary_trees.js
+++ 1.hot100/basic-data-structure/7.symmetric_binary_trees.js
@@ -9,6 +9,7 @@
  * 输出：true
  */
 const check = (p, q) => {
+  debugger
   if (!p && !q) return true;
   if (!p || !q) return false;
   return p.val === q.val && check(p.left, q.right) && check(p.right, q.left);
