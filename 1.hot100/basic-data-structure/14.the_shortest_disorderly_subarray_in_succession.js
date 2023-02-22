88b6e648022ec7976dd2696d057154f7847720ba
zjoney <zdjoey@126.com>
2023-02-22 10:25:50 +0800

    feat: 🎸 answer for the shortest disorder subarray in succession
    https://leetcode.cn/problems/shortest-unsorted-continuous-subarray/submissions/?favorite=2cktkvj

--- 1.hot100/basic-data-structure/14.the_shortest_disorderly_subarray_in_succession.js
+++ 1.hot100/basic-data-structure/14.the_shortest_disorderly_subarray_in_succession.js
@@ -10,27 +10,27 @@
  * 输出：5
  * 解释：你只需要对 [6, 4, 8, 10, 9] 进行升序排序，那么整个表都会变为升序排序。
  */
-var findUnsortedSubarray = function(nums) {
+var findUnsortedSubarray = function (nums) {
   var stack = [];
   var start = Infinity;
   var end = -1;
   var max = -Infinity;
-
-  for (var i=0; i<nums.length; i++) {
-      // 保证栈递增
-      while (stack.length && (nums[stack[stack.length-1]] > nums[i])) {
-          // 保存已发现的最大值
-          max = Math.max(max, nums[stack[stack.length-1]]);
-          var c = stack.pop();
-          // start只能向前移动
-          c < start && (start = c);
-      }
-      // 当前值小于已发现的max，end改为当前
-      if (nums[i] < max) end = i;
-      stack.push(i);
+  debugger
+  for (var i = 0; i < nums.length; i++) {
+    // 保证栈递增
+    while (stack.length && (nums[stack[stack.length - 1]] > nums[i])) {
+      // 保存已发现的最大值
+      max = Math.max(max, nums[stack[stack.length - 1]]);
+      var c = stack.pop();
+      // start只能向前移动
+      c < start && (start = c);
+    }
+    // 当前值小于已发现的max，end改为当前
+    if (nums[i] < max) end = i;
+    stack.push(i);
   }
   if (start > end) return 0;
-  return end-start+1;
+  return end - start + 1;
 };
-const nums = [2,6,4,8,10,9,15]
+const nums = [2, 6, 4, 8, 10, 9, 15]
 console.log(findUnsortedSubarray(nums))
