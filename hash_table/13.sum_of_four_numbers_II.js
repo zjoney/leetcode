8d2b1fc5a834233f664b73fb3dcb9c1518c2139b
zjoney <zdjoey@126.com>
2023-02-22 13:23:28 +0800

    feat: 🎸 answer for sum Of four numbers II
    https://leetcode.cn/problems/4sum-ii/submissions/404904317/

--- hash_table/13.sum_of_four_numbers_II.js
+++ hash_table/13.sum_of_four_numbers_II.js
@@ -2,7 +2,7 @@
  * 考察：哈希表+分组
  * @difficulty中等
  * @summary: 454. 四数相加 II
-给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
+ * 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
 0 <= i, j, k, l < n
 nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
 
