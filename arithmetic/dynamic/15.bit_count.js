68ae07fa86cdacebc2692c3bbbf2b01a72f12212
zjoney <zdjoey@126.com>
2023-02-22 11:09:57 +0800

    feat: 🎸 answer for bit count
    https://leetcode.cn/problems/counting-bits/submissions/404861994/

--- arithmetic/dynamic/15.bit_count.js
+++ arithmetic/dynamic/15.bit_count.js
@@ -3,15 +3,14 @@
  * @difficulty简单
  * @summary: 338. 比特位计数
  * 给你一个整数 n ，对于 0 <= i <= n 中的每个 i ，计算其二进制表示中 1 的个数 ，返回一个长度为 n + 1 的数组 ans 作为答案。
- 
-输入：n = 2
-输出：[0,1,1]
-解释：
-0 --> 0
-1 --> 1
-2 --> 10
-题目理解：
-考察布莱恩 柯宁汉
+ * 输入：n = 2
+ * 输出：[0,1,1]
+ * 解释：
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * 题目理解：
+ * 考察布莱恩 柯宁汉
  */
 //Brian Kernighan 算法 
 var countBits = function (n) {
@@ -24,7 +23,6 @@ var countBits = function (n) {
   }
   return bits
 };
-// 布莱恩 柯宁汉算法
 const countOnes = (x) => {
   debugger;
   let ones = 0;
