8b546cb1d2422a936c63af3865fff88385749adb
zjoney <zdjoey@126.com>
2023-02-22 10:01:39 +0800

    feat: 🎸 answer for totate the image
    https://leetcode.cn/problems/rotate-image/submissions/404827167/?favorite=2cktkvj

--- 1.hot100/basic/6.rotate_the_image.js
+++ 1.hot100/basic/6.rotate_the_image.js
@@ -11,6 +11,7 @@
 var rotate = function(matrix) {
   const n = matrix.length;
   const matrix_new = new Array(n).fill(0).map(() => new Array(n).fill(0));
+  debugger
   for (let i = 0; i < n; i++) {
       for (let j = 0; j < n; j++) {
           matrix_new[j][n - i - 1] = matrix[i][j];
