c4270eebd54a2459f6c98e09baa58e48c6eacb3c
zjoney <zdjoey@126.com>
2023-03-06 09:31:57 +0800

    feat: 🎸 ...update

--- arithmetic/15.finds_firstLast_positions_of_an_element_in_the_sorted_array.js
+++ arithmetic/15.finds_firstLast_positions_of_an_element_in_the_sorted_array.js
@@ -19,7 +19,7 @@ var searchRange = function (nums, target) {
     if (nums[mid] > target) right = mid - 1;
     else left = mid + 1;
   }
-  if (left > right) return [-1, -1];
+  if (left > right) return [-1, -1];debugger
   let i = mid, j = mid;
   while (nums[i] === nums[i - 1]) i--;//向左尝试找相同的元素
   while (nums[j] === nums[j + 1]) j++;//向右尝试找相同的元素
