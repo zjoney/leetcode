5403edb52c3096aee2033e586e80e914b348a23d
zjoney <zdjoey@126.com>
2023-03-10 13:26:26 +0800

    feat: 🎸 Top K Frequent Elements

---
+++ 1.hot100/arithmetic/Top K Frequent Elements.js
@@ -0,0 +1,19 @@
+/**
+ * 考察：
+ * @difficulty 中等
+ * @summary 347. 前 K 个高频元素
+ * 给你一个整数数组 nums 和一个整数 k ，
+ * 请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+ * 示例 1:
+
+输入: nums = [1,1,1,2,2,3], k = 2
+输出: [1,2]
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+ var topKFrequent = function(nums, k) {
+
+};
