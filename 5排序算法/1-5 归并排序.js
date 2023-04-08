2dc00d97b2ede20c8eb348ff5758cebf175062be
zhangjing520 <zdjoey@126.com>
2023-04-08 19:16:16 +0800

    feat: 🎸 归并排序

---
+++ 5排序算法/1-5 归并排序.js
@@ -0,0 +1,37 @@
+/**
+ * 采用分治法
+ * 实现由两种方法：
+
+自上而下的递归（所有递归的方法都可以用迭代重写，所以就有了第 2 种方法）；
+自下而上的迭代；
+ */
+function mergeSort(arr) {  // 采用自上而下的递归方法
+  var len = arr.length;
+  if (len < 2) {
+    return arr;
+  }
+  var middle = Math.floor(len / 2),
+    left = arr.slice(0, middle),
+    right = arr.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  var result = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  while (left.length)
+    result.push(left.shift());
+
+  while (right.length)
+    result.push(right.shift());
+
+  return result;
+}
