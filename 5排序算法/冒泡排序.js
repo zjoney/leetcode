9484a4bcd708464cdd2755f2aa0fad91b0bf80a4
zhangjing520 <zdjoey@126.com>
2023-04-08 15:07:44 +0800

    feat: 🎸 排序-冒泡排序

---
+++ 5排序算法/冒泡排序.js
@@ -0,0 +1,21 @@
+/**
+ * https://www.runoob.com/w3cnote/bubble-sort.html
+ * 比较两个相邻的数据大小，大的就下沉
+ * @param {*} arr 
+ * @returns 
+ */
+function bubbleSort(arr) {
+  var len = arr.length;
+  for (var i = 0; i < len - 1; i++) {
+      for (var j = 0; j < len - 1 - i; j++) {
+          if (arr[j] > arr[j+1]) {        // 相邻元素两两对比
+              var temp = arr[j+1];        // 元素交换
+              arr[j+1] = arr[j];
+              arr[j] = temp;
+          }
+      }
+  }
+  console.log(arr);
+  return arr;
+}
+bubbleSort(['f', 'o', 'o', 'b', 'a', 'r', 'l', 'e', 'd']);
