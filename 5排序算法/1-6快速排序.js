5c53fc17c292f03c1af2b05ffdb794e11fa8eab4
zhangjing520 <zdjoey@126.com>
2023-04-29 06:53:33 +0800

    feat: 🎸 docs

--- 5排序算法/1-6快速排序.js
+++ 5排序算法/1-6快速排序.js
@@ -16,6 +16,8 @@ function partition(arr, low, high) {
   arr[low] = pivot;
   return low;
 }
+// 重复数组的前五个元素作为基准，然后对前五个元素进行9次快速排序
+
 
 function quickSort(arr, low, high) {
   if (low < high) {
@@ -26,4 +28,73 @@ function quickSort(arr, low, high) {
   return arr;
 }
 const arr = [9, 8, 7, 6, 5, 0, 1, 2, 3, 4];
-console.log(quickSort(arr, 0, arr.length - 1))
+// console.log(quickSort(arr, 0, arr.length - 1))
+
+// 计算两个日期之间的差距
+function getDiffDays(date1, date2) {
+  let diff = date2.getTime() - date1.getTime();
+  return Math.floor(diff / (1000 * 60 * 60 * 24));
+}
+
+// 计算两个日期之间的最小时间差
+function getMinutesDiff(date1, date2) {
+  let diff = date2.getTime() - date1.getTime();
+  return Math.floor(diff / (1000 * 60));
+}
+
+// 将日期转换成字符串
+function dateToString(date) { 
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+}
+// 实现手写sort方法
+function mySort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// 实现手写抢红包算法
+function redArrow(numItems, numColors) {
+  let result = [];
+  for (let i = 0; i < numItems; i++) {
+    let color = Math.floor(Math.random() * numColors);
+    result.push(color);
+  }
+  return result;
+}
+console.log('123', redArrow(10, 3));
+
+// leecode题目 两数之和
+var twoSum = function(nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (map.has(diff)) {
+      return [map.get(diff), i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+}
+// 传入两个参数相加之和
+function add(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both arguments must be numbers');
+  }
+
+  return a + b;
+}
+console.log(add(1, 2));
